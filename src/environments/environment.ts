49356deb06ad246686ed970edb77102fbb8877cf
Yasar <yb.budulgan@gmail.com>
2019-06-30 18:44:52 +0300

    Remove credentials
    Old credentials are revoked.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: 'AIzaSyDKvlSRQV8U4-ftqbKM-eIwJQYW9V2ItA0',
-        authDomain: 'sinebox-5a487.firebaseapp.com',
-        databaseURL: 'https://sinebox-5a487.firebaseio.com',
-        projectId: 'sinebox-5a487',
-        storageBucket: 'sinebox-5a487.appspot.com',
-        messagingSenderId: '154369489254'
+        apiKey: 'apiKey',
+        authDomain: 'authDomain',
+        databaseURL: 'databaseURL',
+        projectId: 'projectId',
+        storageBucket: 'storageBucket',
+        messagingSenderId: 'messagingSenderId'
     }
 };
